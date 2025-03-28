f67dc68da2a9c3683058dadeb42c89d7534db626
MikolajSzawerda <mikolaj.szawerda@gmail.com>
2025-03-29 00:58:40 +0100

    add prompt dataset

--- scripts/ablate_musicgen.py
+++ scripts/ablate_musicgen.py
@@ -8,39 +8,64 @@ from musicsae.nnsight_model import MusicGenLanguageModel
 from src.project_config import OUTPUT_DATA_DIR
 from dataclasses import dataclass
 from simple_parsing import parse, Serializable
+from datasets import load_dataset
+import logging
+from uuid import uuid4
+logger = logging.getLogger(__name__)
 #
 # clear_output()
 # # -
 #
 # tokens = 255
 # prompt = "Recreate the essence of a classic video game theme with chiptune sounds and nostalgic melodies."
-# for n in range(0, 20, 2):
-#     ablate_layer = nn_model.decoder.model.decoder.layers[n]
-#     with nn_model.generate(prompt, max_new_tokens=tokens):
-#         outputs = nnsight.list().save() # Initialize & .save() nnsight list
-#         for _ in range(tokens):
-#             ablate_layer.output[0][:] = ablate_layer.input[0][:]
-#             outputs.append(nn_model.generator.output)
-#             nn_model.next()
-#     torchaudio.save(
-#         f"out_{n}.wav",
-#         src=outputs[0][0].detach().cpu(),
-#         sample_rate=nn_model.config.sampling_rate,
-#         channels_first=True,
-#     )
+
 
 @dataclass
 class AblateScriptConfig(Serializable):
     model_name: str = 'small'
     device: str = 'cuda'
+    dataset_name: str = 'amaai-lab/MusicBench'
+    dataset_split: str = 'test'
+    dataset_column: str = 'main_caption'
+    dataset_max_rows: int = 10
+    batch_size: int = 32
+    music_per_prompt: int = 1
+    concept: str = 'anime'
+    ablation_layers: tuple[int] = (20,)
+    num_tokens: int = 255
+
+def get_prompt_batches(cfg: AblateScriptConfig):
+    prompts_ds = load_dataset(cfg.dataset_name, split=cfg.dataset_split).select_columns([cfg.dataset_column])
+    prompts_ds = prompts_ds.select(range(cfg.dataset_max_rows))
+    prompts_ds = prompts_ds.filter(lambda x: cfg.concept in x[cfg.dataset_column])
+    logger.info(f"# of prompts: {prompts_ds.num_rows}")
+    return prompts_ds.batch(cfg.batch_size // cfg.music_per_prompt)
 
 def main():
     args = parse(AblateScriptConfig)
     nn_model = MusicGenLanguageModel(f"facebook/musicgen-{args.model_name}", device_map=args.device)
-
-    # nn_model = MusicGenLanguageModel("facebook/musicgen-small", config=cfg, tokenizer=processor.tokenizer, device_map='cuda')
     with nn_model.generate("Hello world!", max_new_tokens=1):
         ...
+    for layer_id in args.ablation_layers:
+        ablate_layer = nn_model.decoder.model.decoder.layers[layer_id]
+        for batch in get_prompt_batches(args):
+            prompts = batch[args.dataset_column] * args.music_per_prompt
+            with nn_model.generate(prompts, max_new_tokens=args.num_tokens):
+                outputs = nnsight.list().save()  # Initialize & .save() nnsight list
+                for _ in range(args.num_tokens):
+                    ablate_layer.output[0][:] = ablate_layer.input[0][:]
+                    outputs.append(nn_model.generator.output)
+                    nn_model.next()
+            for audio_idx in range(len(prompts)):
+                path = (OUTPUT_DATA_DIR / 'ablate' / args.model_name
+                        / args.dataset_name.split('/')[1]
+                        / args.dataset_split / layer_id / f'out_{str(uuid4())[:6]}.wav')
+                torchaudio.save(
+                    path,
+                    src=outputs[0][path].detach().cpu(),
+                    sample_rate=nn_model.config.sampling_rate,
+                    channels_first=True,
+                )
 
 if __name__ == "__main__":
     main()
