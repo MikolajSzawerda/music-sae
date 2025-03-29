8fa59b1482c792b0c9c8fc991ca0433b8fa963e1
MikolajSzawerda <mikolaj.szawerda@gmail.com>
2025-03-29 01:32:03 +0100

    improve ablation

--- scripts/ablate_musicgen.py
+++ scripts/ablate_musicgen.py
@@ -7,10 +7,11 @@ from IPython.display import clear_output
 from musicsae.nnsight_model import MusicGenLanguageModel
 from src.project_config import OUTPUT_DATA_DIR
 from dataclasses import dataclass
-from simple_parsing import parse, Serializable
+from simple_parsing import parse, Serializable, list_field
 from datasets import load_dataset
 import logging
 from uuid import uuid4
+from tqdm import tqdm
 logger = logging.getLogger(__name__)
 #
 # clear_output()
@@ -31,13 +32,13 @@ class AblateScriptConfig(Serializable):
     batch_size: int = 32
     music_per_prompt: int = 1
     concept: str = 'anime'
-    ablation_layers: tuple[int] = (20,)
+    ablation_layers: list[int] = list_field()
     num_tokens: int = 255
 
 def get_prompt_batches(cfg: AblateScriptConfig):
     prompts_ds = load_dataset(cfg.dataset_name, split=cfg.dataset_split).select_columns([cfg.dataset_column])
-    prompts_ds = prompts_ds.select(range(cfg.dataset_max_rows))
     prompts_ds = prompts_ds.filter(lambda x: cfg.concept in x[cfg.dataset_column])
+    prompts_ds = prompts_ds.select(range(cfg.dataset_max_rows))
     logger.info(f"# of prompts: {prompts_ds.num_rows}")
     return prompts_ds.batch(cfg.batch_size // cfg.music_per_prompt)
 
@@ -48,7 +49,8 @@ def main():
         ...
     for layer_id in args.ablation_layers:
         ablate_layer = nn_model.decoder.model.decoder.layers[layer_id]
-        for batch in get_prompt_batches(args):
+        batches = get_prompt_batches(args)
+        for batch in tqdm(batches, desc=f'l: {layer_id}'):
             prompts = batch[args.dataset_column] * args.music_per_prompt
             with nn_model.generate(prompts, max_new_tokens=args.num_tokens):
                 outputs = nnsight.list().save()  # Initialize & .save() nnsight list
@@ -56,13 +58,14 @@ def main():
                     ablate_layer.output[0][:] = ablate_layer.input[0][:]
                     outputs.append(nn_model.generator.output)
                     nn_model.next()
-            for audio_idx in range(len(prompts)):
-                path = (OUTPUT_DATA_DIR / 'ablate' / args.model_name
+            path = (OUTPUT_DATA_DIR / 'ablate' / args.model_name
                         / args.dataset_name.split('/')[1]
-                        / args.dataset_split / layer_id / f'out_{str(uuid4())[:6]}.wav')
+                        / args.dataset_split / args.concept.replace(' ', '_') / str(layer_id) )
+            path.mkdir(exist_ok=True, parents=True)
+            for audio_idx in range(len(prompts)):
                 torchaudio.save(
-                    path,
-                    src=outputs[0][path].detach().cpu(),
+                    path / f'out_{str(uuid4())[:6]}.wav',
+                    src=outputs[0][audio_idx].detach().cpu(),
                     sample_rate=nn_model.config.sampling_rate,
                     channels_first=True,
                 )
