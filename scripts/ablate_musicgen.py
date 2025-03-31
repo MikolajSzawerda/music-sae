5d97b40ac5f37884e3e67ec801460d2f69af03b6
Patryk Filip Gryz <pfgryz@gmail.com>
2025-03-31 16:18:14 +0200

    Add uv, ruff and format files

--- scripts/ablate_musicgen.py
+++ scripts/ablate_musicgen.py
@@ -11,20 +11,23 @@ from accelerate import PartialState
 import hydra
 from hydra.core.config_store import ConfigStore
 from transformers import set_seed
+
 logger = logging.getLogger(__name__)
+
 
 @dataclass
 class AblateDatasetScriptConfig:
-    name: str = 'amaai-lab/MusicBench'
-    split: str = 'test'
-    column: str = 'main_caption'
+    name: str = "amaai-lab/MusicBench"
+    split: str = "test"
+    column: str = "main_caption"
     max_rows: int = 10
+
 
 @dataclass
 class AblateScriptConfig:
     dataset: AblateDatasetScriptConfig
-    model_name: str = 'small'
-    device: str = 'cuda'
+    model_name: str = "small"
+    device: str = "cuda"
     batch_size: int = 10
     music_per_prompt: int = 1
     ablation_layers: list[int] = list_field()
@@ -32,8 +35,10 @@ class AblateScriptConfig:
     run_with_clean: bool = False
     seed: int = 42
 
+
 cs = ConfigStore.instance()
 cs.store(name="config", node=AblateScriptConfig)
+
 
 def get_prompt_batches(cfg: AblateScriptConfig):
     prompts_ds = load_dataset(cfg.dataset.name, split=cfg.dataset.split).select_columns([cfg.dataset.column])
@@ -41,6 +46,7 @@ def get_prompt_batches(cfg: AblateScriptConfig):
     prompts_ds = prompts_ds.select(range(cfg.dataset.max_rows))
     logger.info(f"# of prompts: {prompts_ds.num_rows}")
     return prompts_ds.batch(cfg.batch_size // cfg.music_per_prompt)
+
 
 @hydra.main(version_base=None, config_path="../conf/ablation", config_name="config")
 def main(args: AblateScriptConfig):
@@ -52,32 +58,44 @@ def main(args: AblateScriptConfig):
         ...
     nn_model.device_map = distributed_state.device
     nn_model.to(distributed_state.device)
-    jobs = list(args.ablation_layers) + (['pure'] if args.run_with_clean else [])
+    jobs = list(args.ablation_layers) + (["pure"] if args.run_with_clean else [])
     with distributed_state.split_between_processes(jobs) as job_idxs:
         for layer_id in job_idxs:
-            path = (OUTPUT_DATA_DIR / 'ablate' / args.model_name
-                    / args.dataset.name.split('/')[1]
-                    / args.dataset.split / str(layer_id))
+            path = (
+                OUTPUT_DATA_DIR
+                / "ablate"
+                / args.model_name
+                / args.dataset.name.split("/")[1]
+                / args.dataset.split
+                / str(layer_id)
+            )
             path.mkdir(exist_ok=True, parents=True)
-            dataloader = tqdm(batches) if distributed_state.is_main_process and distributed_state.local_process_index == 0 else batches
+            dataloader = (
+                tqdm(batches)
+                if distributed_state.is_main_process and distributed_state.local_process_index == 0
+                else batches
+            )
 
             def gen_clean(prompts: list[str]):
                 return nn_model.generate_clean(prompts, args.num_tokens)
+
             def gen_ablate(prompts: list[str]):
                 layer = nn_model.decoder.model.decoder.layers[layer_id]
                 return nn_model.generate_with_ablation(layer, prompts, args.num_tokens)
-            gen_func = gen_clean if layer_id == 'pure' else gen_ablate
+
+            gen_func = gen_clean if layer_id == "pure" else gen_ablate
 
             for batch in dataloader:
                 prompts = batch[args.dataset.column] * args.music_per_prompt
                 outputs = gen_func(prompts)
                 for audio_idx in range(len(prompts)):
                     torchaudio.save(
-                        path / f'out_{str(uuid4())[:6]}.wav',
+                        path / f"out_{str(uuid4())[:6]}.wav",
                         src=outputs[0][audio_idx].detach().cpu(),
                         sample_rate=nn_model.config.sampling_rate,
                         channels_first=True,
                     )
+
 
 if __name__ == "__main__":
     main()
