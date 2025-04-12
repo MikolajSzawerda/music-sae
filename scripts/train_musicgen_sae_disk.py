36bc870f5f50b99b89d11bb7d42e03c7ffeac68d
MikolajSzawerda <mikolaj.szawerda@gmail.com>
2025-04-12 23:31:25 +0200

    better train script

--- scripts/train_musicgen_sae_disk.py
+++ scripts/train_musicgen_sae_disk.py
@@ -1,4 +1,6 @@
 from dictionary_learning.trainers.top_k import AutoEncoderTopK, TopKTrainer
+
+# from dictionary_learning.trainers.top_k import AutoEncoderTopK, TopKTrainer
 from dictionary_learning.training import trainSAE
 from datasets import load_dataset
 from dataclasses import dataclass, field
@@ -22,15 +24,12 @@ class TrainScriptConfig:
     model_name: str = "small"
     device: str = "cuda"
     ablation_layers: list[int] = field(default_factory=list)
-    max_gen_num_tokens: int = 255
     max_steps: int = 1000
-    activation_buffer_size: int = 100
     activation_batch_size: int = 10
     warmup_steps: int = 10
     top_k: int = 5
     seed: int = 42
     sae_size_multiplier: int = 16
-    text_batch_size: int = 10
     log_steps: int = 30
 
 
@@ -40,37 +39,37 @@ cs.store(name="config", node=TrainScriptConfig)
 
 def collate_fn(batch, device):
     activations = torch.tensor([item["activation"] for item in batch])
-    return activations.squeeze().to(device)
+    return activations.squeeze()
+
+
+def infinite_dataloader(loader):
+    while True:
+        for batch in loader:
+            yield batch
 
 
 @hydra.main(version_base=None, config_path="../conf/musicgen-sae", config_name="config")
 def main(args: TrainScriptConfig):
     for layer_id in list(args.ablation_layers):
+        path = INPUT_DATA_DIR / "activation" / args.model_name / "MusicBench" / str(layer_id) / args.dataset.split
         ds = load_dataset(
             "arrow",
-            data_files=str(
-                INPUT_DATA_DIR
-                / "activation"
-                / args.model_name
-                / "MusicBench"
-                / str(layer_id)
-                / args.dataset.split
-                / "*.arrow"
-            ),
+            data_files=[str(path / f"data-{shard_id:05d}-of-99999.arrow") for shard_id in range(23)],
             streaming=True,
             split="train",
         )
+        ds = ds.shuffle(buffer_size=args.activation_batch_size, seed=args.seed)
         dl = DataLoader(
             ds, batch_size=args.activation_batch_size, num_workers=8, collate_fn=lambda x: collate_fn(x, args.device)
         )
-        activation_dim = 1536
+        activation_dim = ds.features["activation"].shape[-1]
         dictionary_size = args.sae_size_multiplier * activation_dim
         trainer_cfg = {
             "trainer": TopKTrainer,
             "dict_class": AutoEncoderTopK,
             "activation_dim": activation_dim,
             "dict_size": dictionary_size,
-            "lr": 1e-3,
+            "lr": 1e-4,
             "device": args.device,
             "steps": args.max_steps,
             "layer": layer_id,
@@ -80,14 +79,16 @@ def main(args: TrainScriptConfig):
             "wandb_name": str(layer_id),
         }
         trainSAE(
-            data=iter(dl),
+            data=infinite_dataloader(dl),
             trainer_configs=[trainer_cfg],
             steps=trainer_cfg["steps"],
-            save_dir=MODELS_DIR / "musicgen-sae" / str(layer_id),
+            save_dir=MODELS_DIR / "musicgen-sae-topk-test" / str(layer_id),
             use_wandb=True,
-            wandb_project="musicgen-sae",
+            wandb_project="musicgen-sae-topk",
             log_steps=args.log_steps,
             normalize_activations=True,
+            verbose=True,
+            save_steps=list(range(args.log_steps, args.max_steps, args.log_steps * 4)),
         )
 
 
