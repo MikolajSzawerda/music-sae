0ddcf580007c8135264b318e472f71a31f9c3ef5
MikolajSzawerda <mikolaj.szawerda@gmail.com>
2025-04-18 23:18:33 +0200

    use plugins with collect

--- src/dataset_plugins.py
+++ src/dataset_plugins.py
@@ -5,15 +5,15 @@ from typing import Generator
 from datasets import Dataset
 
 
-def get_datasets(cfg: dict) -> Generator[Dataset, None, None]:
+def get_datasets(cfg: dict) -> Generator[tuple[Dataset, dict], None, None]:
     registry = {ep.name: ep.load() for ep in im.entry_points(group="ds_plugins")}
     for ds_conf in cfg["datasets"]:
         ds_conf["resample_sr"] = cfg["resample_sr"]
         ds_conf["seed"] = cfg["seed"]
-        cls = registry[ds_conf.pop("name")]
+        cls = registry[ds_conf["name"]]
         postfix = ds_conf.pop("postfix_path", None)
         if postfix:
             ds_conf["base_dir"] = INPUT_DATA_DIR / postfix
         else:
             ds_conf["base_dir"] = Path(ds_conf["base_dir"])
-        yield cls(**ds_conf).prepare(**ds_conf)
+        yield cls(**ds_conf).prepare(**ds_conf), ds_conf
