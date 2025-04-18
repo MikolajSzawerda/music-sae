86d2d74fe1c3437a2b5157728ab7d0064c99a7ea
MikolajSzawerda <mikolaj.szawerda@gmail.com>
2025-04-18 23:52:01 +0200

    mix generation with forward

--- scripts/collect_musicgen_activations.py
+++ scripts/collect_musicgen_activations.py
@@ -49,16 +49,20 @@ def main(args: CollectScriptConfig):
             features = Features({"activation": Array2D(shape=(1, activation_dim), dtype="float32")})
             for ds, ds_cfg in get_datasets(OmegaConf.to_container(args, resolve=True)):
                 name, split = ds_cfg["name"], ds_cfg["split"]
+                gen_audio = "audio_tensor" not in ds.column_names
+                name = name if not gen_audio else f"{name}-gen"
                 path = INPUT_DATA_DIR / "activation-test" / args.model_name / name / str(layer_id) / split
                 path.mkdir(exist_ok=True, parents=True)
                 shard_id, example_counts = 0, 0
                 writer = ArrowWriter(features=features, path=path / f"data-{shard_id:05d}-of-99999.arrow")
                 ds_iter = ds.iter(batch_size=args.collect_batch_size)
-                for batch in (
-                    tqdm.tqdm(ds_iter, total=len(ds) // args.collect_batch_size)
-                    if distributed_state.is_main_process and distributed_state.local_process_index == 0
-                    else ds_iter
-                ):
+
+                def generate_audio(batch):
+                    return nn_model.collect_generation_activations(
+                        layer, batch["main_caption"], args.max_gen_num_tokens
+                    )
+
+                def forward_audio(batch):
                     inputs = processor(
                         audio=batch["audio_tensor"],
                         sampling_rate=32000,
@@ -70,9 +74,15 @@ def main(args: CollectScriptConfig):
                         with nn_model.trace(
                             inputs, invoker_args={"truncation": True, "max_length": args.max_gen_num_tokens}
                         ):
-                            activations = layer.output[0].save()
-                            layer.output.stop()
-                    activations = activations.view(-1, activation_dim).detach().cpu()
+                            return layer.output[0].save()
+
+                gen_activations = generate_audio if gen_audio else forward_audio
+                for batch in (
+                    tqdm.tqdm(ds_iter, total=len(ds) // args.collect_batch_size)
+                    if distributed_state.is_main_process and distributed_state.local_process_index == 0
+                    else ds_iter
+                ):
+                    activations = gen_activations(batch).view(-1, activation_dim).detach().cpu()
                     for act in activations:
                         writer.write({"activation": act.unsqueeze(0).numpy()})
                     example_counts += activations.shape[0]
