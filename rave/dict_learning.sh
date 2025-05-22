77109bb5054a70d6a44948148bb9b590ae5cd4cd
mkielbus <mateusz.kielbus.mk@gmail.com>
2025-05-22 17:00:12 +0200

    Improve loss calculation

--- rave/dict_learning.sh
+++ rave/dict_learning.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 /home/mkielbus/music-sae/rave/.venv/bin/python dictionary_learning.py ./activations/darbouka_decoder_7_30000 \
-  darbouka_decoder_7_30000_50 \
+  darbouka_decoder_7_4_30000_50 \
   50 \
   dict_params.json \
   4 \
-  70000 \
+  1 \
   ./encoded/darbouka_decoder_7_30000/darbouka_decoder_4_50 \
   ./weights/darbouka_decoder_7_4_50_dictionary_weights.npy
--- rave/dictionary_learning.py
+++ rave/dictionary_learning.py
@@ -4,7 +4,6 @@ import numpy as np
 from sklearn.decomposition import MiniBatchDictionaryLearning
 from sklearn.decomposition import sparse_encode
 from sklearn.model_selection import train_test_split
-import sys
 from tqdm import tqdm
 import warnings
 import matplotlib.pyplot as plt
@@ -26,7 +25,7 @@ def getCMDArgs():
     parser.add_argument("epochs", type=int, help="Number of training epochs")
     parser.add_argument("params_filepath", type=str, help="Path to the file containing training hiperparams")
     parser.add_argument("params_id", type=int, help="Hiperparams id")
-    parser.add_argument("remembered_samples", type=int, help="Number of remembered samples for loss computing")
+    parser.add_argument("val_clusters_number", type=int, help="Number of clusters saved for validation loss computing")
     parser.add_argument("output_path", type=str, help="first part of the path to the file with encoded tensors")
     parser.add_argument("weights_path", type=str, help="Path to the file with weights for the dictionary")
     args = parser.parse_args()
@@ -34,19 +33,19 @@ def getCMDArgs():
 
 
 def loss(dictionary: np.ndarray, fit_algorithm: str, n_nonzero_coefs: int | None, alpha: float,
-         train_losses: list, val_losses: list, X_train: np.ndarray, X_val: np.ndarray):
-    for X, losses in [(X_train, train_losses), (X_val, val_losses)]:
-        U = sparse_encode(X, dictionary=dictionary, algorithm=fit_algorithm, n_nonzero_coefs=n_nonzero_coefs,
-                          alpha=alpha)
-        reconstruction = U @ dictionary
-        frob_loss = 0.5 * ((X - reconstruction) ** 2).sum()
-        l1_loss = alpha * np.sum(np.abs(U))
-        total_loss = frob_loss + l1_loss
-        losses.append(total_loss)
+         losses: list, dataset: np.ndarray):
+    U = sparse_encode(dataset, dictionary=dictionary, algorithm=fit_algorithm, n_nonzero_coefs=n_nonzero_coefs,
+                      alpha=alpha)
+    reconstruction = U @ dictionary
+    frob_loss = 0.5 * ((dataset - reconstruction) ** 2).sum()
+    l1_loss = alpha * np.sum(np.abs(U))
+    total_loss = frob_loss + l1_loss
+    losses.append(total_loss)
 
 
-def makeCallback(progress_bar: tqdm):
+def makeCallback(progress_bar: tqdm, batch_losses: list[float]):
     def callback(args):
+        batch_losses.append(args["batch_cost"])
         progress_bar.total = args["n_steps"]
         progress_bar.update(1)
         progress_bar.set_description(f"Dictionary training, Batch loss: {args['batch_cost']:.4f}")
@@ -89,7 +88,7 @@ def getMiniBatchDictLearningParamsList(id: int, filepath: str) -> dict:
     return [(id, to_return_params_dict[id])]
 
 
-def train(params_id: int, params_filepath: str, weights_filepath: str, epochs: int, memory_window_length: int,
+def train(params_id: int, params_filepath: str, weights_filepath: str, epochs: int, X_val: np.ndarray,
           base_name: str,
           files_paths: list[str]) -> list[MiniBatchDictionaryLearning]:
     dictionaries = []
@@ -100,8 +99,9 @@ def train(params_id: int, params_filepath: str, weights_filepath: str, epochs: i
         epochs_bar = tqdm(range(epochs), position=0, leave=True, dynamic_ncols=True)
         dictionaries.append(trainEpochs(training_params=training_params, params_id=params_id, files_paths=files_paths,
                                         weights_filepath=weights_filepath,
-                                        train_losses=train_losses, val_losses=val_losses, components=components,
-                                        epochs_bar=epochs_bar, epochs=epochs, memory_window_length=memory_window_length,
+                                        train_losses=train_losses, val_losses=val_losses,
+                                        components=components,
+                                        epochs_bar=epochs_bar, epochs=epochs, X_val=X_val,
                                         base_name=base_name))
     return dictionaries
 
@@ -110,22 +110,24 @@ def trainEpochs(training_params: dict, params_id: int, files_paths: list[str], w
                 train_losses: list[float],
                 val_losses: list[float],
                 components: np.ndarray | None, epochs_bar: tqdm, epochs: int,
-                memory_window_length: int, base_name: str) -> MiniBatchDictionaryLearning:
+                X_val: np.ndarray,
+                base_name: str) -> MiniBatchDictionaryLearning:
     dictionary = None
     with epochs_bar:
         for epoch_number in range(epochs):
-            remembered_samples, dictionary = trainEpoch(training_params=training_params, files_paths=files_paths,
-                                                        weights_filepath=weights_filepath,
-                                                        components=components, epochs_bar=epochs_bar,
-                                                        epoch_number=epoch_number,
-                                                        memory_window_length=memory_window_length)
+            batch_losses = []
+            dictionary = trainEpoch(training_params=training_params, files_paths=files_paths,
+                                    weights_filepath=weights_filepath,
+                                    batch_losses=batch_losses,
+                                    components=components, epochs_bar=epochs_bar,
+                                    epoch_number=epoch_number)
             components = dictionary.components_
-            X_train, X_val = train_test_split(remembered_samples, test_size=0.2, random_state=42)
+            train_losses.append(sum(batch_losses))
             loss(dictionary.components_, dictionary._fit_algorithm,
                  dictionary.transform_n_nonzero_coefs, dictionary.alpha,
-                 train_losses, val_losses, X_train, X_val)
+                 val_losses, X_val)
             epochs_bar.update(1)
-            tqdm.write(f"Epoch number: {epoch_number}, Train loss: {train_losses[-1]:.4f} Val loss:\
+            tqdm.write(f"Epoch number: {epoch_number}, Train loss: {train_losses[-1]:.4f} Val loss: \
 {val_losses[-1]:.4f}")
             saveLossPlots(train_losses, val_losses, "diagrams/" + base_name +
                           f"_loss_params_id_{params_id}.png")
@@ -137,48 +139,31 @@ def trainEpochs(training_params: dict, params_id: int, files_paths: list[str], w
     return dictionary
 
 
-def trainEpoch(training_params: dict, files_paths: list[str], weights_filepath: str,
-               components: np.ndarray | None, epochs_bar: tqdm, epoch_number: int,
-               memory_window_length: int) -> tuple[np.ndarray, MiniBatchDictionaryLearning]:
+def trainEpoch(training_params: dict, files_paths: list[str], weights_filepath: str, batch_losses: list[float],
+               components: np.ndarray | None, epochs_bar: tqdm, epoch_number: int) -> MiniBatchDictionaryLearning:
     epochs_bar.set_description_str(f"Dictionary training, Epoch number {epoch_number}")
-    remembered_samples = None
     batch_clusters_bar = tqdm(total=len(files_paths), position=1, leave=False, dynamic_ncols=True,
                               desc="Dictionary training over batch cluster number")
-    remembered_samples, dictionary = trainBatchClusters(training_params, components=components,
-                                                        batch_clusters_bar=batch_clusters_bar,
-                                                        memory_window_length=memory_window_length,
-                                                        remembered=remembered_samples,
-                                                        files_paths=files_paths, weights_filepath=weights_filepath)
-    return remembered_samples, dictionary
+    dictionary = trainBatchClusters(training_params, components=components,
+                                    batch_clusters_bar=batch_clusters_bar,
+                                    batch_losses=batch_losses,
+                                    files_paths=files_paths, weights_filepath=weights_filepath)
+    return dictionary
 
 
 def trainBatchClusters(training_params: dict, components: np.ndarray | None, batch_clusters_bar: tqdm,
-                       memory_window_length: int, remembered: np.ndarray | None,
-                       files_paths: list[str], weights_filepath: str) -> tuple[np.ndarray, MiniBatchDictionaryLearning]:
+                       batch_losses: list[float],
+                       files_paths: list[str], weights_filepath: str) -> MiniBatchDictionaryLearning:
     dictionary = None
     with batch_clusters_bar:
         for cluster_number, file_path in enumerate(files_paths):
             batch_clusters_bar.set_description_str(f"Dictionary training over batch cluster number: \
 {cluster_number}")
             X = loadActivations(file_path)
-            if remembered is None:
-                remembered = np.ndarray(X.shape)
-            n_to_add_rows = X.shape[0]
-            n_rows = remembered.shape[0] + X.shape[0]
-            if n_rows > memory_window_length:
-                if remembered.shape[0] == memory_window_length:
-                    remembered = remembered[n_to_add_rows:]
-                    remembered = np.vstack([remembered, X])
-                else:
-                    remembered = np.vstack([remembered, X[:n_rows - memory_window_length]])
-                    remembered = remembered[n_to_add_rows - (n_rows - memory_window_length):]
-                    remembered = np.vstack([remembered, X[n_rows - memory_window_length:]])
-            else:
-                remembered = remembered = np.vstack([remembered, X])
             progress_bar = tqdm(position=2, leave=False, dynamic_ncols=True,
                                 desc="Dictionary training, Batch loss")
             dictionary = MiniBatchDictionaryLearning(**training_params,
-                                                     callback=makeCallback(progress_bar),
+                                                     callback=makeCallback(progress_bar, batch_losses),
                                                      dict_init=components)
             with progress_bar:
                 dictionary = dictionary.fit(X)
@@ -186,7 +171,7 @@ def trainBatchClusters(training_params: dict, components: np.ndarray | None, bat
             np.save(weights_filepath, dictionary.components_)
             components = dictionary.components_
             batch_clusters_bar.update(1)
-    return remembered, dictionary
+    return dictionary
 
 
 def encode(dictionaries: list[MiniBatchDictionaryLearning], files_paths: list[str], output_path: str) -> None:
@@ -201,14 +186,27 @@ def encode(dictionaries: list[MiniBatchDictionaryLearning], files_paths: list[st
             torch.save(X_coded, output_path + file_path.stem + f'_{dict_number}_.pt')
 
 
+def prepareValidationSet(files_paths: list[str], val_clusters_number: int, seed: int) -> tuple[list[str], np.ndarray]:
+    val_files_paths = []
+    new_files_paths, val_files_paths = train_test_split(files_paths, test_size=val_clusters_number, random_state=seed)
+    X_val = None
+    for file_path in val_files_paths:
+        if X_val is None:
+            X_val = loadActivations(file_path)
+        else:
+            X_val = np.vstack([X_val, loadActivations(file_path)])
+    return new_files_paths, X_val
+
+
 def main():
     warnings.filterwarnings("ignore")
     args = getCMDArgs()
     files_paths = findPtFiles(args.activations_path)
+    files_paths, X_val = prepareValidationSet(files_paths, args.val_clusters_number, seed=42)
     dictionaries = train(params_id=args.params_id, params_filepath=args.params_filepath,
                          weights_filepath=args.weights_path,
                          epochs=args.epochs,
-                         memory_window_length=args.remembered_samples,
+                         X_val=X_val,
                          base_name=args.base_name, files_paths=files_paths)
     print("Learning finished. Starting encoding")
     for dictionary in dictionaries:
