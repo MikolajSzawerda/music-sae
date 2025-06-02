93e6b50f25c3cd0821a0f96d9ac391496346c9f4
Patryk Filip Gryz <pfgryz@gmail.com>
2025-06-02 23:58:35 +0200

    update dependencies

--- rave/dict_learning.sh
+++ rave/dict_learning.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/home/mkielbus/music-sae/rave/.venv/bin/python dictionary_learning.py ./activations/darbouka_decoder_7_30000 \
+./music-sae/rave/.venv/bin/python dictionary_learning.py ./activations/darbouka_decoder_7_30000 \
   darbouka_decoder_7_4_30000_50 \
   50 \
   dict_params.json \
